8ea26ec6325548832d9e563cf0b8f3b519daa859
luckyboy <qingyunzou@u.nus.edu>
2025-09-14 19:54:00 +0800

    Fix HLS array partition directives in aes256_encrypt_ecb

--- benchmark/MachSuite/aes/aes/aes_new.c
+++ benchmark/MachSuite/aes/aes/aes_new.c
@@ -181,8 +181,8 @@ void aes_expandEncKey(uint8_t *k, uint8_t *rc)
 /* -------------------------------------------------------------------------- */
 void aes256_encrypt_ecb(aes256_context *ctx, uint8_t k[32], uint8_t buf[16])
 {
-#pragma HLS array_partition variable=buf type=cyclic factor=2  // set_directive_array_partition -factor 2 -type cyclic aes256_encrypt_ecb buf
-#pragma HLS array_partition variable=k type=cyclic factor=2  // set_directive_array_partition -factor 2 -type cyclic aes256_encrypt_ecb k
+#pragma HLS array_partition variable=buf type=cyclic factor=2 dim=1 // set_directive_array_partition -factor 2 -type cyclic aes256_encrypt_ecb buf
+#pragma HLS array_partition variable=k type=cyclic factor=2  dim=1 // set_directive_array_partition -factor 2 -type cyclic aes256_encrypt_ecb k
     //INIT
     uint8_t rcon = 1;
     uint8_t i;
